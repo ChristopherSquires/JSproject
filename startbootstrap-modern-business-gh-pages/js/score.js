db7dac81eca455e9c8ca631de4c1a4e747a5c615
Christopher <Christopher.Squires43@ed.cna.nl.ca>
2024-12-16 14:11:09 -0330

    score updated

--- startbootstrap-modern-business-gh-pages/js/score.js
+++ startbootstrap-modern-business-gh-pages/js/score.js
@@ -3,4 +3,6 @@
 const $ = selector => document.querySelector(selector);
 
 let score = localStorage.getItem("score");
-$("#display").textContent = `${score}/10`;
+$("#display").textContent = `Score: ${score}/10`;
+
+$("#answers").textContent = "ANSWERS: Porygon, Pokemon Legends Z-A, Rhydon, Mewtwo, Tera, 15, Liko, Garchomp, 48, Pachirisu";
